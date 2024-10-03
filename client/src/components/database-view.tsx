9239f7c61d3f8eceb70d834ab509f3229004f621
mahutt <tommy.mahut@gmail.com>
2024-10-03 18:21:47 -0400

    Add share database button

--- client/src/components/database-view.tsx
+++ client/src/components/database-view.tsx
@@ -1,6 +1,8 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Button } from '@/components/ui/button'
 import TableView, { Table } from '@/components/table'
 import useLocalStorage from 'use-local-storage'
+import { Share1Icon } from '@radix-ui/react-icons'
 
 export interface Database {
   tables: Table[]
@@ -12,29 +14,37 @@ export default function DatabaseView({ database }: { database: Database }) {
     database.tables[0]?.name ?? 'none'
   )
   return (
-    <Tabs
-      defaultValue={database.tables[0]?.name ?? 'none'}
-      value={table}
-      onValueChange={setTable}
-      className="w-full shadow-sm border border-gray-200 rounded-xl p-4"
-    >
-      <TabsList>
+    <>
+      <div className="flex flex-row gap-2 my-4">
+        <Button variant="ghost" className="text-gray-500">
+          <Share1Icon className="w-4 h-4 mr-1" />
+          Share
+        </Button>
+      </div>
+      <Tabs
+        defaultValue={database.tables[0]?.name ?? 'none'}
+        value={table}
+        onValueChange={setTable}
+        className="w-full shadow-sm border border-gray-200 rounded-xl p-4"
+      >
+        <TabsList>
+          {database.tables.map((table) => (
+            <TabsTrigger key={table.name} value={table.name}>
+              {table.name}
+            </TabsTrigger>
+          ))}
+          {database.tables.length === 0 && (
+            <TabsTrigger value="none" disabled>
+              No tables
+            </TabsTrigger>
+          )}
+        </TabsList>
         {database.tables.map((table) => (
-          <TabsTrigger key={table.name} value={table.name}>
-            {table.name}
-          </TabsTrigger>
+          <TabsContent key={table.name} value={table.name}>
+            <TableView table={table} />
+          </TabsContent>
         ))}
-        {database.tables.length === 0 && (
-          <TabsTrigger value="none" disabled>
-            No tables
-          </TabsTrigger>
-        )}
-      </TabsList>
-      {database.tables.map((table) => (
-        <TabsContent key={table.name} value={table.name}>
-          <TableView table={table} />
-        </TabsContent>
-      ))}
-    </Tabs>
+      </Tabs>
+    </>
   )
 }
